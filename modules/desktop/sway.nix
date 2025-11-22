9ee5a07c292bb1500edc945384618b0b3c612fb2
brandonlau6 <brandonctx0@gmail.com>
2025-11-22 16:53:04 -0500

    minor gtk changes

--- modules/desktop/sway.nix
+++ modules/desktop/sway.nix
@@ -48,8 +48,8 @@
   
   gtk.enable = true;
 
-  gtk.cursorTheme.package = pkgs.bibata-cursors;
-  gtk.cursorTheme.name = "Bibata-Modern-Ice";
+  #gtk.cursorTheme.package = pkgs.bibata-cursors;
+  #gtk.cursorTheme.name = "Bibata-Modern-Ice";
 
   gtk.theme.package = pkgs.adw-gtk3;
   gtk.theme.name = "adw-gtk3-dark";
