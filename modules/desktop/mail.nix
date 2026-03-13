886219842ea6a6a844afb6995c4b6eb170b8eabe
brandonlau6 <brandonctx0@gmail.com>
2026-03-13 12:03:40 -0400

    aws cli

--- modules/desktop/mail.nix
+++ modules/desktop/mail.nix
@@ -4,7 +4,29 @@
     sopsFile = ../../secrets/gmail1.yaml;
   };
 
-  
+  accounts.email = {
+    accounts.gmail1 = {
+      address = "brandonctx0@gmail.com";
+      imap.host = "imap.gmail.com";
+      mbsync = {
+        enable = true;
+        create = "maildir";
+      };
+      msmtp.enable = true;
+      notmuch.enable = true;
+      primary = true;
+      realName = "Brandon Lau";
+      passwordCommand = "cat /run/user/1000/secrets/gmail1";
+      smtp = {
+        host = "smtp.gmail.com";
+      };
+      userName = "brandonctx0@gmail.com";
+
+      thunderbird = {
+        enable = true;
+      };
+    };
+  };
 
 
 }
