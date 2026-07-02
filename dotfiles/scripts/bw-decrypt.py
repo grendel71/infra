a0be530fbd4e35e6eabc1abd1742c467198d0467
brandonlau6 <brandonctx0@gmail.com>
2026-07-01 23:49:12 -0400

    fix(bw-decrypt): mark click options as required to prevent None args

--- dotfiles/scripts/bw-decrypt.py
+++ dotfiles/scripts/bw-decrypt.py
@@ -1,9 +1,9 @@
 import click
 import subprocess
 @click.command()
-@click.option('--file', help='input file')
-@click.option('--password', help='password')
-@click.option('--output', help='output')
+@click.option('--file', required=True, help='input .enc file')
+@click.option('--password', required=True, help='decryption password')
+@click.option('--output', required=True, help='output .json file')
 def decrypt(file, password, output):
     subprocess.run(
         [
